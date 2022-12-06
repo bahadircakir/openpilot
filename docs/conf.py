502c837096a5f48a60e3ada04d42c43acebe591a
bahadircakir <49199356+bahadircakir@users.noreply.github.com>
2022-12-06 14:57:56 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -137,7 +137,7 @@ html_show_copyright = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/logo.png'
+html_logo = 'https://github.com/bahadircakir/openpilot/blob/master/selfdrive/assets/img_spinner_comma.png'
 html_favicon = '_static/favicon.ico'
 html_theme_options = {
   'logo_only': False,
